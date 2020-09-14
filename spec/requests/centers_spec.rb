b55ab354d4553300ace8852a7efb6f3054ed7c38
uche-inyama <depoulet@yahoo.com>
2020-09-14 17:54:10 +0100

    resolve rubocop errors

--- spec/requests/centers_spec.rb
+++ spec/requests/centers_spec.rb
@@ -4,22 +4,23 @@ require 'pp'
 RSpec.describe '/centers', type: :request do
   let(:valid_attributes) do
     { building: 'Avalon',
-      hall: 'Brisk', 
+      hall: 'Brisk',
       city: 'Imogeneborough',
-      state: 'California', 
-      price: 1200, 
+      state: 'California',
+      price: 1200,
       capacity: 300,
       image: 'http://res.cloudinary.com/ddcakt97r/image/upload/v1599940689/event_center/meta-studio-35-PJXAjKduyjQ-unsplash_onzzzc.jpg'
     }
   end
 
   let(:invalid_attributes) do
-    { building: '',
-      hall: '', 
+    { 
+      building: '',
+      hall: '',
       city: '',
-      state: '', 
-      price: '', 
-      capacity: '', 
+      state: '',
+      price: '',
+      capacity: '',
       image: ''
     }
   end
