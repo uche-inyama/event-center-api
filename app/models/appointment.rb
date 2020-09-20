e176393ff0d9a5bb57cd083628acb0e9e3753964
uche-inyama <depoulet@yahoo.com>
2020-09-20 03:57:46 +0100

    correct rubocop errors

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,13 +1,13 @@
 class Appointment < ApplicationRecord
-  belongs_to :user
-  belongs_to :center
-  validates :username, presence: true,
-                        uniqueness: true
+  belongs_to  :user
+  belongs_to  :center
+  validates   :username,  presence: true,
+                          uniqueness: true
 
   validates :city, presence: true
   validates :hall, presence: true
 
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i.freeze
   validates :email, presence: true,
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
