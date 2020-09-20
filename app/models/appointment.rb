719fae673f3e49b66d98d4203d1ddbe3d85ce887
uche-inyama <depoulet@yahoo.com>
2020-09-20 03:29:54 +0100

    add association between models

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,4 +1,6 @@
 class Appointment < ApplicationRecord
+  belongs_to :user
+  belongs_to :center
   validates :username, presence: true
   validates :email, presence: true
   validates :city, presence: true
