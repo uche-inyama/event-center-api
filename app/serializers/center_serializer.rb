eb43bdf3b498458228d30122c3061ae9a32c95f3
uche-inyama <depoulet@yahoo.com>
2020-09-28 10:45:12 +0100

    update serializer file

--- app/serializers/center_serializer.rb
+++ app/serializers/center_serializer.rb
@@ -1,4 +1,4 @@
 class CenterSerializer < ActiveModel::Serializer
   include Rails.application.routes.url_helpers
-  attributes :id, :building, :hall, :price, :capacity, :city, :state, :image, :date
+  attributes :id, :building, :hall, :price, :capacity, :city, :state, :image
 end
