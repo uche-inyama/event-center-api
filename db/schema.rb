b1803a358e5e403a205a27cb16f4ff88927ef043
uche-inyama <depoulet@yahoo.com>
2020-09-28 06:42:43 +0100

    add date column to appointments table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_12_144023) do
+ActiveRecord::Schema.define(version: 2020_09_28_041439) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 2020_09_12_144023) do
     t.string "city"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.date "date"
   end
 
   create_table "centers", force: :cascade do |t|
