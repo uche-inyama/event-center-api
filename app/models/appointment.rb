65cdc2e971b64ad6d4d9efff23812b995b6fb6db
uche-inyama <depoulet@yahoo.com>
2020-09-29 13:18:04 +0100

    remove trailing commas

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -11,6 +11,6 @@ class Appointment < ApplicationRecord
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i.freeze
   validates :email, presence: true,
-                    format: { with: VALID_EMAIL_REGEX },
+                    format: { with: VALID_EMAIL_REGEX }
                     # uniqueness: { case_sensitive: false }
 end
