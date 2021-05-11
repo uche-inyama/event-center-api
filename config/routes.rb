5bad3756f2825fb07e1f5b0cb160295cf5c2561a
uche-inyama <depoulet@yahoo.com>
2021-05-11 11:26:47 +0100

    add attributes for center & appointment

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  # post "/graphql", to: "graphql#execute"
   resources :images, only: %i[create]
   resources :sessions, only: %i[create]
   resources :registrations, only: %i[create]
