70d733103d17fea7a089e756f49a0c8a733379f5
uche-inyama <depoulet@yahoo.com>
2022-01-25 11:58:05 +0100

    updating cors: making origin more restrictive

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -9,11 +9,11 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins ['https://jovial-meitner-20063a.netlify.app', 'http://localhost:3000']
 
-    resource '/api/v1/*',
+    resource '*',
       headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+      methods: [:get, :post, :put, :patch, :delete, :options, :head],
       credentials: true
   end
 end
