4d37ca9333958feb50da229f210ee48bbad42700
uche-inyama <depoulet@yahoo.com>
2022-01-25 11:52:23 +0100

    update cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -9,10 +9,11 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://jovial-meitner-20063a.netlify.app'
+    origins '*'
 
-    resource '*',
-        headers: :any,
-        methods: %i[get post put patch delete options head],
+    resource '/api/v1/*',
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+      credentials: true
   end
 end
