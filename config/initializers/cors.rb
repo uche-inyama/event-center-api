ee8bfe69ee8aec1e6e660d08137d28a26bedaa22
uche-inyama <depoulet@yahoo.com>
2022-01-25 23:31:56 +0100

    update cors file to accept request from all origin

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -9,7 +9,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ['https://jovial-meitner-20063a.netlify.app', 'http://localhost:3001']
+    origins '*'
 
     resource '*',
       headers: :any,
