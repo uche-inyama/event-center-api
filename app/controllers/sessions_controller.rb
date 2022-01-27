ac6071a866cb0b97b7b1a0e1c946bebca848fa0d
uche-inyama <depoulet@yahoo.com>
2022-01-27 07:03:21 +0100

    corrections made on sessions  controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@ class SessionsController < ApplicationController
     user = User.find_by(username: params['user']['username'])
     if user
       session[:user_id] = user.id
-      render json:  currentUser: user, logged_in: true, status: :created
+      render json:  { currentUser: user, logged_in: true, status: :created }
     else
       render json: status: 401
     end
@@ -11,9 +11,9 @@ class SessionsController < ApplicationController
 
   def logged_in
     if @current_user
-      render json: logged_in: true, current_user: @current_user
+      render json: { logged_in: true, current_user: @current_user }
     else
-      render json: logged_in: false
+      render json: { logged_in: false }
     end
   end
 
