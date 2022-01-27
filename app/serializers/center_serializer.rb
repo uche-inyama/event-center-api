77c382f6208961aefae08bc690b1b1b3185ee645
uche-inyama <depoulet@yahoo.com>
2022-01-27 11:18:25 +0100

    comment out active model serializers file

--- app/serializers/center_serializer.rb
+++ app/serializers/center_serializer.rb
@@ -1,4 +1,4 @@
-class CenterSerializer < ActiveModel::Serializer
-  include Rails.application.routes.url_helpers
-  attributes :id, :building, :hall, :address, :price, :capacity, :city, :state, :image
-end
+# class CenterSerializer < ActiveModel::Serializer
+#   include Rails.application.routes.url_helpers
+#   attributes :id, :building, :hall, :address, :price, :capacity, :city, :state, :image
+# end
